28087b2759d0edf6fdab0e9541b96650f3220d71
Interrobang <113810873+Interrobang01@users.noreply.github.com>
2024-07-19 16:45:51 -0700

    updated hinge tool with shift, alt, and ctrl modifiers

--- hinge_tool/src/main.lua
+++ hinge_tool/src/main.lua
@@ -1,10 +1,7 @@
 local start = vec2(0,0)
 local target_1_position = vec2(0, 0)
 local target_1_color = nil
-local prev_shape_guid = nil
 local target_1_guid = nil
-
-local prev_pointer_pos = vec2(0, 0);
 
 function on_update()
     if Input:pointer_just_pressed() then
@@ -13,10 +10,6 @@ function on_update()
     if Input:pointer_just_released() then
         on_pointer_up(Input:pointer_pos());
     end;
-    if Input:pointer_pos() ~= prev_pointer_pos then
-        on_pointer_move(Input:pointer_pos());
-    end;
-    prev_pointer_pos = Input:pointer_pos();
 end;
 
 function on_pointer_down(point)
@@ -32,14 +25,30 @@ function on_pointer_down(point)
                 radius = 0,
             }
             if #selected_objects == 0 then return end
+
+            local snap = Input:key_pressed("ControlLeft") or Input:key_pressed("ControlRight")
+            local start_at_center = Input:key_pressed("AltLeft") or Input:key_pressed("AltRight")
+
             local target_1 = selected_objects[1]
             local target_1_color = target_1.color
             target_1.color = (0xffffff - target_1.color) -- temporary color reassignment algorithm. change later
 
+            local target_1_position = target_1:get_position()
+            local start = input.point
+            if start_at_center then start = target_1_position end
+
+            if snap then
+                start = start * 2
+                start = start+vec2(0.5,0.5)
+                start = vec2(math.floor(start.x),math.floor(start.y))
+                start = start / 2
+            end
+
             return {
-                target_1_position = target_1:get_position(),
+                target_1_position = target_1_position,
                 target_1_color = target_1_color,
-                target_1_guid = target_1.guid
+                target_1_guid = target_1.guid,
+                start = start
             }
         ]]
     }
@@ -48,45 +57,7 @@ function on_pointer_down(point)
         target_1_position = output.target_1_position
         target_1_color = output.target_1_color
         target_1_guid = output.target_1_guid
-    end
-end
-
-function on_pointer_move(point)
-    print("Pointer move at " .. point.x .. ", " .. point.y)
-    if start_marker_guid ~= nil then
-        print('start marker wasnt nil, realing up')
-        local output = runtime_eval{
-            input = {
-                start_point = start,
-                end_point = point,
-                guid = target_1_guid,
-                prev_shape_guid = prev_shape_guid,
-            },
-            code = [[
-                if input.prev_shape_guid ~= nil then
-                    Scene:get_object_by_guid(input.prev_shape_guid):destroy()
-                end
-
-                local line_pos = (input.start_point+input.end_point)/2
-                local line_sx = (input.start_point-input.end_point):magnitude()
-                local relative_line_end = input.end_point-line_pos
-                local line_rotation = math.atan(relative_line_end.y/relative_line_end.x)
-                local line = Scene:add_box({
-                    position = line_pos,
-                    size = vec2(line_sx/2, 0.25),
-                    is_static = true,
-                    color = 0x695662
-                });
-                line:set_angle(line_rotation)
-
-                return {
-                    guid = line.guid
-                }
-            ]]
-        }
-        if output ~= nil then
-            prev_shape_guid = output.guid
-        end
+        start = output.start
     end
 end
 
@@ -98,15 +69,10 @@ function on_pointer_up(point)
                 start_point = start,
                 end_point = point,
                 target_1_guid = target_1_guid,
-                prev_shape_guid = prev_shape_guid,
                 target_1_color = target_1_color,
                 target_1_position = target_1_position,
             },
             code = [[
-                if input.prev_shape_guid ~= nil then
-                    print('about to destroy prev_shape_guid ' .. tostring(input.prev_shape_guid))
-                    Scene:get_object_by_guid(input.prev_shape_guid):destroy()
-                end
 
                 local selected_objects = Scene:overlap_circle{
                     position = input.end_point,
@@ -119,14 +85,24 @@ function on_pointer_up(point)
 
                 if #selected_objects == 0 then return end
 
-                target_1:set_position(input.end_point + (input.target_1_position-input.start_point))
-                target_1:set_linear_velocity(input.end_point-input.start_point) -- crude way to unsleep objects
+                local snap = Input:key_pressed("ControlLeft") or Input:key_pressed("ControlRight")
+                local motor = Input:key_pressed("ShiftLeft") or Input:key_pressed("ShiftRight")
+                local end_at_center = Input:key_pressed("AltLeft") or Input:key_pressed("AltRight")
+
+                local adjusted_end_point = input.end_point
+
+                if snap then
+                    adjusted_end_point = adjusted_end_point * 2
+                    adjusted_end_point = adjusted_end_point+vec2(0.5,0.5)
+                    adjusted_end_point = vec2(math.floor(adjusted_end_point.x),math.floor(adjusted_end_point.y))
+                    adjusted_end_point = adjusted_end_point / 2
+                end
 
                 local target_2 = selected_objects[1]
                 if target_2 == target_1 then
                     if #selected_objects == 1 then
                         target_2 = Scene:add_circle{
-                            position = input.end_point,
+                            position = adjusted_end_point,
                             radius = 0.01,
                             is_static = true,
                             color = Scene.background_color,
@@ -137,21 +113,23 @@ function on_pointer_up(point)
                     end
                 end
 
+                if end_at_center then adjusted_end_point = target_2:get_position() end
+
+                target_1:set_position(adjusted_end_point + (input.target_1_position-input.start_point))
+                target_1:set_linear_velocity((adjusted_end_point-input.start_point)/10) -- crude way to unsleep objects. also, feel.
+
 
 
                 local hinge = Scene:add_hinge_at_world_point{
-                    point = input.end_point,
+                    point = adjusted_end_point,
                     object_a = target_1,
                     object_b = target_2,
-                    motor_enabled = false,
-                    motor_speed = 1, -- radians per second
-                    max_motor_torque = 10, -- maximum torque for the motor, in newton-meters
+                    motor_enabled = motor,
+                    motor_speed = math.pi*2, -- radians per second
+                    max_motor_torque = 100, -- maximum torque for the motor, in newton-meters
                 }
                 ]]
         }
     end
-    prev_shape_guid = nil
-    start_marker_guid = nil
-    end_marker_guid = nil
     target_1_guid = nil
 end

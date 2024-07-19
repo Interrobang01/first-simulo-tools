1acffc14b6b2fe6ebedb5eefd768c7b5e025364a
Interrobang <113810873+Interrobang01@users.noreply.github.com>
2024-07-18 21:13:04 -0700

    static tool: made holding shift freeze all objects under cursor, and made alt unfreeze them

--- static_toggle/src/main.lua
+++ static_toggle/src/main.lua
@@ -1,15 +1,15 @@
 function on_update()
-    if Input:pointer_just_pressed() then
-        on_pointer_down(Input:pointer_pos())
+    if Input:pointer_just_pressed() or Input:key_pressed("ShiftLeft") or Input:key_pressed("AltLeft") then
+        on_pointer_down(Input:pointer_pos(),Input:key_pressed("ShiftLeft"),Input:key_pressed("AltLeft"))
     end
 end
 
-function on_pointer_down(point)
-    print("Pointer down at " .. point.x .. ", " .. point.y)
-
+function on_pointer_down(point, shift, alt)
     runtime_eval{
         input = {
             point = point,
+            shift = shift,
+            alt = alt,
         },
         code = [[
 local objects_in_circle = Scene:overlap_circle{
@@ -17,13 +17,22 @@ local objects_in_circle = Scene:overlap_circle{
     radius = 0,
 }
 
-if objects_in_circle[1] ~= nil then
-    local obj = objects_in_circle[1]
-
+local function toggle(obj)
     local is_static = obj:temp_get_is_static()
+    if input.shift then is_static = false end
+    if input.alt then is_static = true end
     obj:temp_set_is_static(not is_static)
-else
-    print("no hit")
+end
+
+if objects_in_circle[1] ~= nil then
+    if shift == false and alt == false then
+        toggle(objects_in_circle[1])
+    else
+        for i,obj in ipairs(objects_in_circle) do
+            toggle(obj)
+            print("e")
+        end
+    end
 end
         ]]
     }

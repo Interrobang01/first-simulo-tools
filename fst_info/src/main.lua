110638d4cf498e71c6bd2eefb5b5f414d68d0b43
Interrobang <113810873+Interrobang01@users.noreply.github.com>
2024-09-06 21:45:09 -0700

    updated info tool to work with new builtin console

--- fst_info/src/main.lua
+++ fst_info/src/main.lua
@@ -1,11 +1,10 @@
 function on_update()
     if Input:pointer_just_pressed() then
         on_pointer_down(Input:pointer_pos())
-    end;
-end;
+    end
+end
 
 function on_pointer_down(point)
-    print("Position is "..tostring(point))
     runtime_eval{
         input = {
             point = point,
@@ -15,15 +14,20 @@ local objects_in_circle = Scene:get_objects_in_circle{
     position = input.point,
     radius = 0,
 }
-
+Console:log("Pos is "..tostring(input.point))
 if #objects_in_circle > 0 then
     for i = 1,#objects_in_circle do
-        print(" ")
-        print("Object name is "..objects_in_circle[i]:get_name())
-        print("Object GUID is "..tostring(objects_in_circle[i].guid))
-        print("Object position is "..tostring(objects_in_circle[i]:get_position()))
+        local obj = objects_in_circle[i]
+        local name = obj:get_name()
+        local objcolor = obj:get_color()
+        Console:log(" ")
+        Console:log("Object name is "..name)
+        Console:log("GUID is "..tostring(obj.guid))
+        Console:log("position is "..tostring(obj:get_position()))
+        Console:log("body type is "..tostring(obj:get_body_type()))
+        Console:log("color is "..tostring(objcolor.r).." "..tostring(objcolor.g).." "..tostring(objcolor.b).." "..tostring(objcolor.a))
     end
 end
         ]]
     }
-end;
+end

2235a33a534016f25692d4f711fe3d8c9b4855fc
Interrobang <113810873+Interrobang01@users.noreply.github.com>
2024-09-06 21:05:27 -0700

    updated static tool for 0.10

--- fst_static_toggle/src/main.lua
+++ fst_static_toggle/src/main.lua
@@ -1,15 +1,19 @@
 function on_update()
-    if Input:pointer_just_pressed() or Input:key_pressed("ShiftLeft") or Input:key_pressed("AltLeft") then
-        on_pointer_down(Input:pointer_pos(),Input:key_pressed("ShiftLeft"),Input:key_pressed("AltLeft"))
+    local shift = Input:key_pressed("ShiftLeft") or Input:key_pressed("ShiftRight")
+    local alt = Input:key_pressed("AltLeft") or Input:key_pressed("AltRight")
+    local control = Input:key_pressed("ControlLeft") or Input:key_pressed("ControlRight")
+    if Input:pointer_just_pressed() or shift or alt or control then
+        on_pointer_down(Input:pointer_pos(),shift,alt,control)
     end
 end
 
-function on_pointer_down(point, shift, alt)
+function on_pointer_down(point, shift, alt, control)
     runtime_eval{
         input = {
             point = point,
             shift = shift,
             alt = alt,
+            control = control,
         },
         code = [[
 local objects_in_circle = Scene:get_objects_in_circle{
@@ -18,10 +22,18 @@ local objects_in_circle = Scene:get_objects_in_circle{
 }
 
 local function toggle(obj)
-    local is_static = obj:temp_get_is_static()
-    if input.shift then is_static = false end
-    if input.alt then is_static = true end
-    obj:temp_set_is_static(not is_static)
+    local current_body_type = obj:get_body_type()
+    local target_body_type = nil
+    if current_body_type == BodyType.Static then
+        target_body_type = BodyType.Dynamic
+    end
+    if current_body_type == BodyType.Dynamic or current_body_type == BodyType.Kinematic then
+        target_body_type = BodyType.Static
+    end
+    if input.shift then target_body_type = BodyType.Static end
+    if input.alt then target_body_type = BodyType.Dynamic end
+    if input.control then target_body_type = BodyType.Kinematic end
+    obj:set_body_type(target_body_type)
 end
 
 if objects_in_circle[1] ~= nil then
